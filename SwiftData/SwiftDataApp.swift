1aabf957a3f6543bbaad37baa9823ee61fa7d276
Sara Samreen <sarasamreen73@gmail.com>
2026-07-06 15:21:33 +0500

    Initial Commit

---
+++ SwiftData/SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataApp.swift
+//  SwiftData
+//
+//  Created by Mac on 06/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
